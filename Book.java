868d0f0f14064e13913b442ef0b862cb3cba16c5
Кира Белькова <kirabelkova2013@yandex.ru>
2024-03-25 11:47:50 +0300

    добавила класс Book

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private T title;
+    public Book(T title) {
+        this.title = title;
+    }
+    
+    public T getTitle() {
+        return title;
+    }
+    }
+    
